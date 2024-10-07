4b3163bafc3f583ab7086c7ea6441ca021d6e095
K L <keqingli1129@gmail.com>
2024-10-06 19:21:54 -0500

    first source commit

---
+++ signtotext/ethan.py
@@ -0,0 +1,10 @@
+
+def helloworld(name):
+#   """This function greets the person passed in as a parameter."""
+  print("Hello, " + name + "!")
+  print("Hello, " + helloworld + "!")
+
+         
+#write a function that takes a name as a parameter and prints "Hello, " + name + "!"
+
+  
