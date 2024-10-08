1fa3fcc174a497bc326e0d1836f9b5128ed80940
K L <keqingli1129@gmail.com>
2024-10-08 14:38:07 -0500

    remove tssrc folder

--- tssrc/src/app/pages/translate/signed-to-spoken/signed-language-input/signed-language-input.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-signed-language-input',
-  templateUrl: './signed-language-input.component.html',
-  styleUrl: './signed-language-input.component.scss',
-})
-export class SignedLanguageInputComponent {}
--- tssrc/src/app/pages/translate/spoken-to-signed/spoken-to-signed.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-spoken-to-signed',
-  templateUrl: './spoken-to-signed.component.html',
-  styleUrls: ['./spoken-to-signed.component.scss'],
-})
-export class SpokenToSignedComponent {}
