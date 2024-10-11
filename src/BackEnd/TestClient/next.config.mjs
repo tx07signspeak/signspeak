7103376570d4b08578500398ef0183bd069553c1
K L <keqingli1129@gmail.com>
2024-10-10 20:49:40 -0500

    remove react client

--- src/BackEnd/TestClient/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  transpilePackages: ["three"],
-};
-
-export default nextConfig;
--- src/BackEnd/TestClient/src/app/translate/components/Expressive.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function Expressive() {
-  return (
-    <div className="flex flex-1 flex-col items-center overflow-hidden bg-neutral-900"></div>
-  );
-}
