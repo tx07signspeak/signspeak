b26cc4d419edb1c4969221b2b075dae5d85e9bd1
K L <keqingli1129@gmail.com>
2024-10-20 08:58:21 -0500

    server streaming

--- src/BackEnd/server.py
+++ src/BackEnd/server.py
@@ -27,6 +27,8 @@ app = Flask(__name__)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+firstFrame = True
+
 @app.route("/")
 def index():
     return render_template('index.html')  # Render an HTML template
@@ -200,7 +202,7 @@ def generate_frames():
                     keypoint_classifier_labels[hand_sign_id],
                     point_history_classifier_labels[most_common_fg_id[0][0]],
                 )
-                print('Text: ' + keypoint_classifier_labels[hand_sign_id])
+                # print('Text: ' + keypoint_classifier_labels[hand_sign_id])
                 # print('Gesture Text: ' + point_history_classifier_labels[most_common_fg_id[0][0]])
                 socketio.send(keypoint_classifier_labels[hand_sign_id])
         else:
@@ -217,38 +219,180 @@ def generate_frames():
 
     camera.release()
     cv.destroyAllWindows()
-@socketio.on('video-frame')
-def handle_video_frame(data):
-    # Decode the Base64-encoded frame data
-    client_id = request.sid
-    print(f'Data from {client_id}')
-    image = base64_to_image(data)
-    # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    # Process the frame as needed
-    # ...
-    # Example processing (convert to grayscale)
-    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    
-    # Encode processed image back to base64
-    _, buffer = cv.imencode('.jpg', gray_image)
-    encoded_image = base64.b64encode(buffer).decode('utf-8')
-    socketio.emit('display-frame', f"data:image/jpeg;base64,{encoded_image}")
-    
-def base64_to_image(base64_string):
-    # Extract the base64 encoded binary data from the input string
-    base64_data = base64_string.split(",")[1]
-    # Decode the base64 data to bytes
-    image_bytes = base64.b64decode(base64_data)
-    # Convert the bytes to numpy array
-    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
-    # Decode the numpy array as an image using OpenCV
-    image = cv.imdecode(image_array, cv.IMREAD_COLOR)
-    # filename = os.path.join(f'image_{int(time.time())}.jpg')
+# @socketio.on('video-frame')
+# def handle_video_frame(data):
+#     # Decode the Base64-encoded frame data
+#     # client_id = request.sid
+#     # print(f'Data from {client_id}')
+#     global firstFrame
+#     # hands: mp.python.solutions.hands.Hands
+#     mode = 0
+#     use_brect: bool
+#     keypoint_classifier: KeyPointClassifier
+#     point_history_classifier: PointHistoryClassifier
+#     keypoint_classifier_labels: list[str]
+#     point_history_classifier_labels: list[str]
+#     history_length: int
+#     point_history: deque
+#     finger_gesture_history: deque
+#     # Initialize MediaPipe Hands
+#     mp_hands = mp.solutions.hands
+#     # Create an instance of Hands with default configurations
+#     hands = mp_hands.Hands()
+#     if firstFrame :
+#         firstFrame = False
+#         args = uf.get_args()
 
-    # Save the image
-    # cv.imwrite(filename, image)
-    return image  
-  
+#         # cap_device = args.device
+#         # cap_width = args.width
+#         # cap_height = args.height
+
+#         use_static_image_mode = args.use_static_image_mode
+#         min_detection_confidence = args.min_detection_confidence
+#         min_tracking_confidence = args.min_tracking_confidence
+
+#         use_brect = True
+#         # Model load #############################################################
+#         mp_hands = mp.solutions.hands
+#         hands = mp_hands.Hands(
+#             static_image_mode=use_static_image_mode,
+#             max_num_hands=1,
+#             min_detection_confidence=min_detection_confidence,
+#             min_tracking_confidence=min_tracking_confidence,
+#         )
+
+#         keypoint_classifier = KeyPointClassifier()
+
+#         point_history_classifier = PointHistoryClassifier()
+
+#         # Read labels ###########################################################
+#         with open('model/keypoint_classifier/keypoint_classifier_label.csv',
+#                 encoding='utf-8-sig') as f:
+#             keypoint_classifier_labels = csv.reader(f)
+#             keypoint_classifier_labels = [
+#                 row[0] for row in keypoint_classifier_labels
+#             ]
+#         with open(
+#                 'model/point_history_classifier/point_history_classifier_label.csv',
+#                 encoding='utf-8-sig') as f:
+#             point_history_classifier_labels = csv.reader(f)
+#             point_history_classifier_labels = [
+#                 row[0] for row in point_history_classifier_labels
+#             ]
+#         # FPS Measurement ########################################################
+#         cvFpsCalc = CvFpsCalc(buffer_len=10)
+
+#         # Coordinate history #################################################################
+#         history_length = 16
+#         point_history = deque(maxlen=history_length)
+
+#         # Finger gesture history ################################################
+#         finger_gesture_history = deque(maxlen=history_length)
+
+#         #  ########################################################################
+#         print('First thing first')
+#     else:
+#         image_in = base64_to_image(data)
+#         if image_in is None:
+#             pass
+#         else:   
+#             # print('processing...')
+#             image = cv.flip(image_in, 1)  # Mirror display
+#             # fps = cvFpsCalc.get()
+#             # Process Key (ESC: end) #################################################
+#             key = cv.waitKey(10)
+            
+#             number, mode = uf.select_mode(key, mode)
+#             debug_image = copy.deepcopy(image)
+
+#             # Detection implementation #############################################################
+#             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+#             image.flags.writeable = False
+#             results = hands.process(image)
+#             image.flags.writeable = True
+
+#                 #  ####################################################################
+#             if results.multi_hand_landmarks is not None:
+#                 for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+#                                                         results.multi_handedness):
+#                         # Bounding box calculation
+#                         brect = uf.calc_bounding_rect(debug_image, hand_landmarks)
+#                         # Landmark calculation
+#                         landmark_list = uf.calc_landmark_list(debug_image, hand_landmarks)
+
+#                         # Conversion to relative coordinates / normalized coordinates
+#                         pre_processed_landmark_list = uf.pre_process_landmark(
+#                             landmark_list)
+#                         pre_processed_point_history_list = uf.pre_process_point_history(
+#                             debug_image, point_history)
+#                         # Write to the dataset file
+#                         uf.logging_csv(number, mode, pre_processed_landmark_list,
+#                                     pre_processed_point_history_list)
+
+#                         # Hand sign classification
+#                         hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+#                         if hand_sign_id == 2:  # Point gesture
+#                             point_history.append(landmark_list[8])
+#                         else:
+#                             point_history.append([0, 0])
+
+#                         # Finger gesture classification
+#                         finger_gesture_id = 0
+#                         point_history_len = len(pre_processed_point_history_list)
+#                         if point_history_len == (history_length * 2):
+#                             finger_gesture_id = point_history_classifier(
+#                                 pre_processed_point_history_list)
+
+#                         # Calculates the gesture IDs in the latest detection
+#                         finger_gesture_history.append(finger_gesture_id)
+#                         most_common_fg_id = Counter(
+#                             finger_gesture_history).most_common()
+
+#                         # Drawing part
+#                         debug_image = uf.draw_bounding_rect(use_brect, debug_image, brect)
+#                         debug_image = uf.draw_landmarks(debug_image, landmark_list)
+#                         debug_image = uf.draw_info_text(
+#                             debug_image,
+#                             brect,
+#                             handedness,
+#                             keypoint_classifier_labels[hand_sign_id],
+#                             point_history_classifier_labels[most_common_fg_id[0][0]],
+#                         )
+#                         # print('Text: ' + keypoint_classifier_labels[hand_sign_id])
+#                         # print('Gesture Text: ' + point_history_classifier_labels[most_common_fg_id[0][0]])
+#                         socketio.send(keypoint_classifier_labels[hand_sign_id])
+#                 else:
+#                     point_history.append([0, 0])
+    
+    
+#     # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#     # Process the frame as needed
+#     # ...
+#     # # Example processing (convert to grayscale)
+#     # gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    
+#     # # Encode processed image back to base64
+#     # _, buffer = cv.imencode('.jpg', gray_image)
+#     # encoded_image = base64.b64encode(buffer).decode('utf-8')
+#     # socketio.emit('display-frame', f"data:image/jpeg;base64,{encoded_image}")
+    
+# def base64_to_image(base64_string):
+#     try:
+#         # Extract the base64 encoded binary data from the input string
+#         base64_data = base64_string.split(",")[1]
+#         # Decode the base64 data to bytes
+#         image_bytes = base64.b64decode(base64_data)
+#         # Convert the bytes to numpy array
+#         image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+#         # Decode the numpy array as an image using OpenCV
+#         image = cv.imdecode(image_array, cv.IMREAD_COLOR)
+#         return image  
+#     except:
+#         return None
+#     # filename = os.path.join(f'image_{int(time.time())}.jpg')
+#     # Save the image
+#     # cv.imwrite(filename, image)
 @app.route('/stream')
 def stream():
     return render_template('stream.html')
