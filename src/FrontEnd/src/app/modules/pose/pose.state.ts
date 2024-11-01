ae06b48838d83e4fa7aa0810fa1979ee4f82f470
K L <keqingli1129@gmail.com>
2024-11-01 14:43:56 -0500

    all working now :)

--- src/FrontEnd/src/app/modules/pose/pose.state.ts
+++ src/FrontEnd/src/app/modules/pose/pose.state.ts
@@ -1,9 +1,11 @@
-import {Injectable} from '@angular/core';
+import {Injectable, ɵChangeDetectionScheduler} from '@angular/core';
 import {Action, NgxsOnInit, State, StateContext, Store} from '@ngxs/store';
 import {PoseService} from './pose.service';
 import {LoadPoseEstimationModel, PoseVideoFrame, StoreFramePose} from './pose.actions';
 import * as tf from '@tensorflow/tfjs';
-
+import {
+  SetSpokenLanguageText,
+} from '../../modules/translate/translate.actions';
 export interface PoseLandmark {
   x: number;
   y: number;
@@ -30,6 +32,14 @@ const initialState: PoseStateModel = {
   isLoaded: false,
   pose: null,
 };
+// Define our labelmap
+const labelMap = {
+    1:{name:'Hello', color:'red'},
+    2:{name:'Thank You', color:'yellow'},
+    3:{name:'I Love You', color:'lime'},
+    4:{name:'Yes', color:'blue'},
+    5:{name:'No', color:'purple'},
+}
 
 @Injectable()
 @State<PoseStateModel>({
@@ -42,11 +52,15 @@ export class PoseState implements NgxsOnInit {
   private modelLoaded: boolean = false;
   // loadPromise must be static, in case multiple PoseService instances are created (during testing)
   static loadPromise: Promise<any>;
+  private lastText: string = '';
   constructor(private poseService: PoseService, private store: Store) {}
 
-  ngxsOnInit(): void {
-    this.initializeTensorFlow();
-    this.loadModel();
+  async ngxsOnInit(): Promise<void> {
+    await this.initializeTensorFlow();
+    await this.loadModel();
+    const throttledProcessFrame = this.throttle(imageData => {
+      this.Detect(imageData);
+    }, 16.7); // Adjust the delay (1000 ms = 1 second) as needed
     this.poseService.onResults(results => {
       // TODO: passing the `image` canvas through NGXS bugs the pose. (last verified 2024/02/28)
       // https://github.com/google/mediapipe/issues/2422
@@ -67,13 +81,34 @@ export class PoseState implements NgxsOnInit {
         })
       );
       const imageData = ctx.getImageData(0, 0, fakeImage.width, fakeImage.height);
-      const img = tf.browser.fromPixels(imageData);
-      const resized = tf.image.resizeBilinear(img, [640, 480]);
-      const casted = resized.cast('int32');
-      const expanded = casted.expandDims(0);
-      const obj = this.DetectModel.executeAsync(expanded);
-      console.log(obj);
+      throttledProcessFrame(imageData);
     });
+  }
+  private async Detect(imageData: ImageData) {
+    const img = tf.browser.fromPixels(imageData);
+    const resized = tf.image.resizeBilinear(img, [640, 480]);
+    const casted = resized.cast('int32');
+    const expanded = casted.expandDims(0);
+    const obj = await this.DetectModel.executeAsync(expanded);
+    const boxes = await obj[1].array(); // Bounding boxes
+    const classes = await obj[2].array(); // Class labels
+    const scores = await obj[4].array(); // Confidence scores
+    for (let i = 0; i <= boxes[0].length; i++) {
+      if (boxes[0][i] && classes[0][i] && scores[0][i] > 0.8) {
+        const text = classes[0][i];
+        const data = labelMap[text]['name'];
+        console.log(data);
+        if (data !== this.lastText) {
+          this.store.dispatch(new SetSpokenLanguageText(data));
+          this.lastText = data;
+        }
+      }
+    }
+    tf.dispose(img);
+    tf.dispose(resized);
+    tf.dispose(casted);
+    tf.dispose(expanded);
+    tf.dispose(obj);
   }
   private async initializeTensorFlow() {
     console.log('Initializing TensorFlow.js...');
@@ -113,6 +148,18 @@ export class PoseState implements NgxsOnInit {
       this.isModelLoading = false;
     }
   }
+
+  private throttle(func, delay) {
+    let lastCall = 0;
+    return function (...args) {
+      const now = new Date().getTime();
+      if (now - lastCall >= delay) {
+        lastCall = now;
+        func(...args);
+      }
+    };
+  }
+
   @Action(LoadPoseEstimationModel)
   async loadPose(): Promise<void> {
     await this.poseService.load();
